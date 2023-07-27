9735e9942d663344b1f56f18dff7413f11be5b47
rafaelsene01 <rafaelssene@gmail.com>
2023-07-27 11:26:09 -0300

    contagem de erro

--- src/service/tjgo.ts
+++ src/service/tjgo.ts
@@ -89,7 +89,7 @@ export const tjgo = async (req, res) => {
 
     await queuePaginacao.drained();
 
-    res.status(200).send({ total: processList.length, items: processList });
+    res.status(200).send({ total: processList.length, error: processList.filter(i => !i.processNumber).length, items: processList });
   } catch (error) {
     res.status(400).send({ message: "Erro ao buscar" });
   }
